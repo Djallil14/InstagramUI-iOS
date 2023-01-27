d7060db1e723aa94c023ec3402a36ee6334b0acb
Djallil Elkebir <elkebir.djallil@icloud.com>
2023-01-26 22:53:47 -0500

    Initial Commit

---
+++ InstagramUIClone/InstagramUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUICloneApp.swift
+//  InstagramUIClone
+//
+//  Created by Djallil on 2023-01-26.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
